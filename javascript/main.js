5671e94374c21c32b5bbc5e5fdcb3f72c4ade0a8
asiya <mujawarasiya7@gmail.com>
2018-02-15 14:56:20 +0530

    Files to learn HTML

---
+++ javascript/main.js
@@ -0,0 +1,33 @@
+
+    function print(asiya)
+    {
+        console.log("hello" + arguments[0]);
+    document.write(person.id + person.name)
+    }
+
+    function add(a,b)
+    {
+        c = a + b;
+    console.log(c);
+ console.log(a);
+ console.log(b);
+    }
+
+    function multiply(a,b)
+    {
+        c = a * b;
+    console.log(c);
+    }
+    var sub= function(a,b)
+    {
+
+        c = a - b;
+    console.log(c);
+
+    }
+
+    var person={
+        id: 2,
+        name:"asiya",
+    }
+
